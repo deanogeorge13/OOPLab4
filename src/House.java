679ee7b6bb5dd8831d228f6bddf5c0e8dd118bc8
Dean D. George <Dean.D.George@students.ittralee.ie>
2018-10-16 15:42:29 +0100

    Initial commit

---
+++ src/House.java
@@ -0,0 +1,67 @@
+public class House {
+    private String address;
+    private String type;
+    private double price;
+    private Person owner;
+
+    public House()
+    {
+        setAddress("No Address Specified");
+        setType("No Type Specified");
+        setPrice(0.0);
+        setOwner(new Person("No Owner Specified"));
+    }
+
+    public House(String addr, String type, double price, String name, int age, char gender)
+    {
+        this.address=addr;
+        this.type=type;
+        this.price=price;
+        this.owner = new Person(name, age, gender);
+    }
+
+    public String getAddress()
+    {
+        return address;
+    }
+
+    public String getType()
+    {
+        return type;
+    }
+
+    public double getPrice()
+    {
+        return price;
+    }
+
+    public Person getOwner()
+    {
+        return owner;
+    }
+
+    public void setAddress(String address)
+    {
+        this.address = address;
+    }
+
+    public void setType(String type)
+    {
+        this.type = type;
+    }
+
+    public void setPrice(double price)
+    {
+        this.price = price;
+    }
+
+    public void setOwner(Person owner)
+    {
+        this.owner = owner;
+    }
+
+    public String toString()
+    {
+        return "Address: " + getAddress() + "\nType: " + getType() + "\nPrice: " + getPrice() + "\nOwner: " + getOwner();
+    }
+}
---
+++ src/House1.java
@@ -0,0 +1,5 @@
+public class House1 {
+    public static void main(String[] args) {
+
+    }
+}
